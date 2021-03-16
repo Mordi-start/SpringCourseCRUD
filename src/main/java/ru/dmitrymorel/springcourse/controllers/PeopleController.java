0e2ee57e95ff4b19aa9ecd0cd0959c33aa3cdaf0
dmitrymorel <dimc1998@gmail.com>
2021-03-16 11:39:51 +0300

    Lesson 23

--- src/main/java/ru/dmitrymorel/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/dmitrymorel/springcourse/controllers/PeopleController.java
@@ -52,5 +52,11 @@ public class PeopleController {
         personDAO.update(id, person);
         return "redirect:/people";
     }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
 }
 
